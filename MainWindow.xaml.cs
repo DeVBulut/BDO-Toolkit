f2cd6e122b0dff7274bc7bef64232fc9c64a5520
Emirhan <125173937+DeVBulut@users.noreply.github.com>
2025-08-05 15:37:48 -0600

    Navigation Window & Button overhaul
    Added a navigation menu,
    Added buttons for Various Tools,
    Added color codes for each window.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Bdo_Toolkit;
 
@@ -16,9 +17,16 @@ namespace Bdo_Toolkit;
 /// </summary>
 public partial class MainWindow : Window
 {
+
+    private DispatcherTimer _timer;
+    private TimeSpan _elapsed;
+
     public MainWindow()
     {
-        
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += Timer_Tick;
+        _elapsed = TimeSpan.Zero;
     }
 
     private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -27,5 +35,28 @@ public partial class MainWindow : Window
         {
             this.DragMove();
         }
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        _elapsed = _elapsed.Add(TimeSpan.FromSeconds(1));
+        TimerTextBlock.Text = $"Timer: {_elapsed:hh\\:mm\\:ss}";
+    }
+
+    private void Start_Click(object sender, RoutedEventArgs e)
+    {
+        _timer.Start();
+    }
+
+    private void Pause_Click(object sender, RoutedEventArgs e)
+    {
+        _timer.Stop();
+    }
+
+    private void End_Click(object sender, RoutedEventArgs e)
+    {
+        _timer.Stop();
+        _elapsed = TimeSpan.Zero;
+        TimerTextBlock.Text = "Timer: 00:00:00";
     }
 }
